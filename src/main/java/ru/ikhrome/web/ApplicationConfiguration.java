d85b7904ce210a25908c3fa71f767fee5264bc46
Ivan Khromov <khromov.ivan@gmail.com>
2016-08-20 15:20:38 +0500

    Add beans for Hibernate
    Includes: transactionManager, sessionFactory and dataSource. Waiting for DAO beans now

--- src/main/java/ru/ikhrome/web/ApplicationConfiguration.java
+++ src/main/java/ru/ikhrome/web/ApplicationConfiguration.java
@@ -1,12 +1,19 @@
 package ru.ikhrome.web;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -22,4 +29,32 @@ public class ApplicationConfiguration {
         viewResolver.setSuffix(".jsp");
         return viewResolver;
     }
+
+    @Bean(name = "dataSource")
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/iklog");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123123");
+        return dataSource;
+    }
+
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) {
+        LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+
+        sessionBuilder.scanPackages("ru.ikhrome.model");
+        sessionBuilder.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+
+        return sessionBuilder.buildSessionFactory();
+    }
+
+    @Autowired
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager getHibernateTransactionManager(SessionFactory sessionFactory) {
+
+        return new HibernateTransactionManager(sessionFactory);
+    }
+
 }
