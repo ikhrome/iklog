57a675920ea7dcd368f9651ef66af6d74a10b166
Ivan Khromov <khromov.ivan@gmail.com>
2016-08-20 10:25:00 +0500

    Add testing support and add HomeController test mock

---
+++ src/test/java/ru/ikhrome/controller/HomeControllerTest.java
@@ -0,0 +1,21 @@
+package ru.ikhrome.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class HomeControllerTest {
+    @Test
+    public void getHome() throws Exception {
+
+    }
+
+    @Test
+    public void getAbout() throws Exception {
+
+    }
+
+}
