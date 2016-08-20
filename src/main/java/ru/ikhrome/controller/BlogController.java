e1c0d90c335ad850b779b2d3d4c18708d8a359cf
Ivan Khromov <khromov.ivan@gmail.com>
2016-08-20 16:45:59 +0500

    Create BlogController view methods and define RequestMapping

--- src/main/java/ru/ikhrome/controller/BlogController.java
+++ src/main/java/ru/ikhrome/controller/BlogController.java
@@ -1,8 +1,34 @@
 package ru.ikhrome.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.ikhrome.dao.PostDAO;
+import ru.ikhrome.model.Post;
+
+import java.util.List;
 
 @Controller
 public class BlogController {
-    // TODO: Add methods here
+
+    @Autowired
+    private PostDAO postDAO;
+
+    @RequestMapping("/blog")
+    public String getBlogHome(Model model) {
+        // TODO: add pagination
+        List<Post> postList = postDAO.list();
+        model.addAttribute("posts", postList);
+        return "blog/home";
+    }
+
+    @RequestMapping(value = "/blog/post/{id}", method = RequestMethod.GET)
+    public String getBlogPost(@PathVariable("id") int id, Model model) {
+        Post post = postDAO.get(id);
+        model.addAttribute("post", post);
+        return "blog/post";
+    }
 }
