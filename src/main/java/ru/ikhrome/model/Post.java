e3cf17814bbaa0d558f69c917fcce97761d7274c
Ivan Khromov <khromov.ivan@gmail.com>
2016-08-20 14:58:24 +0500

    Generate getters and setters methods for Post and Category model

--- src/main/java/ru/ikhrome/model/Post.java
+++ src/main/java/ru/ikhrome/model/Post.java
@@ -38,4 +38,75 @@ public class Post {
     @Column(name = "published")
     private Date published;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getMetaWords() {
+        return metaWords;
+    }
+
+    public void setMetaWords(String metaWords) {
+        this.metaWords = metaWords;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getPublished() {
+        return published;
+    }
+
+    public void setPublished(Date published) {
+        this.published = published;
+    }
 }
